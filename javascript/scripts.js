88e2568e95656eba67aa4d3d23926ca57cc774e3
nalab <nalaboficial@gmail.com>
2024-08-22 14:10:01 -0300

    update
    working page theme button (dark theme and light theme), centered image

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,3 +4,31 @@ function scrollToSection(event, sectionId) {
         behavior: 'smooth' 
     });
 }
+
+function pageTheme() {
+    const icon = document.getElementById('theme-icon');
+    icon.classList.add('fade-out');
+
+    setTimeout(() => {
+        const currentIcon = icon.textContent.trim();
+
+        const header = document.querySelector('header');
+        const body = document.querySelector('body');
+        const geral = document.documentElement;
+
+        if (currentIcon === 'dark_mode') {
+            icon.textContent = 'light_mode';
+            header.style = "background-color: #e6e6e6"
+            body.style = "background-color: #f3f3f3"
+            geral.style.setProperty('--main-color', 'black')
+
+        } else {
+            icon.textContent = 'dark_mode';
+            header.style = "background-color: #070707"
+            body.style = "background-color: #0C0C0C"
+            geral.style.setProperty('--main-color', 'white')
+        }
+
+        icon.classList.remove('fade-out');
+    }, 100);
+}
