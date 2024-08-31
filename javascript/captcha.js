f6e6eb48f65ce3eaa277f1b8400ba9400c81d846
nalab <nalaboficial@gmail.com>
2024-08-31 13:30:02 -0300

    recaptcha fix

--- javascript/captcha.js
+++ javascript/captcha.js
@@ -1,14 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var form = document.getElementById('contact-form');
-    if (form) {
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
-            grecaptcha.ready(function() {
-                grecaptcha.execute('6LcUWjMqAAAAAPaIV31IjoyyZDVcJvA1XUKUc0zf', {action: 'submit'}).then(function(token) {
-                    document.getElementById('recaptcha-token').value = token;
-                    form.submit();
-                });
-            });
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LcUWjMqAAAAAPaIV31IjoyyZDVcJvA1XUKUc0zf', {action: 'submit'}).then(function(token) {
+            document.getElementById('recaptcha-token').value = token;
+
+            document.getElementById('contact-form').submit();
         });
-    }
+    });
 });
