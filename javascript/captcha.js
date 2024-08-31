034b061f007242a459a64ecbf06e73c9500cf459
nalab <nalaboficial@gmail.com>
2024-08-31 10:10:13 -0300

    captcha fix
    reCAPTCHA implementation fix

--- javascript/captcha.js
+++ javascript/captcha.js
@@ -1,3 +1,13 @@
-function onSubmit(token) {
-    document.getElementById("contact-form").submit();
-}
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Evita o envio padrão do formulário
+
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LcUWjMqAAAAAPaIV31IjoyyZDVcJvA1XUKUc0zf', {action: 'submit'}).then(function(token) {
+            // Adiciona o token ao campo oculto
+            document.getElementById('recaptcha-token').value = token;
+
+            // Agora pode submeter o formulário
+            document.getElementById('contact-form').submit();
+        });
+    });
+});
